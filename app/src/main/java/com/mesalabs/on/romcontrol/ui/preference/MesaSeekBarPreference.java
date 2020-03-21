70431d31aa3764e1784e8476643fb0693e25e7b3
BlackMesa123 <giangrecosalvo9@gmail.com>
2020-03-21 20:59:45 +0100

    app/rc-preference: add custom SeekBarPreference
    Signed-off-by: BlackMesa123 <giangrecosalvo9@gmail.com>

--- app/src/main/java/com/mesalabs/on/romcontrol/ui/preference/MesaSeekBarPreference.java
+++ app/src/main/java/com/mesalabs/on/romcontrol/ui/preference/MesaSeekBarPreference.java
@@ -1,9 +1,19 @@
 package com.mesalabs.on.romcontrol.ui.preference;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.TextView;
 
-import com.samsung.android.ui.preference.SeslSeekBarPreference;
+import com.mesalabs.on.romcontrol.R;
+import com.mesalabs.on.romcontrol.utils.LogUtils;
+import com.samsung.android.ui.preference.PreferenceViewHolder;
+import com.samsung.android.ui.preference.SeslPreference;
+import com.samsung.android.ui.widget.SeslSeekBar;
 
 /*
  * On Settings
@@ -17,16 +27,320 @@ import com.samsung.android.ui.preference.SeslSeekBarPreference;
  *      http://www.apache.org/licenses/LICENSE-2.0
  */
 
-public class MesaSeekBarPreference extends SeslSeekBarPreference {
-    public MesaSeekBarPreference(Context var1, AttributeSet var2) {
-        super(var1, var2);
+public class MesaSeekBarPreference extends SeslPreference {
+    private static final String TAG = "MesaSeekBarPreference";
+    @SuppressWarnings("WeakerAccess")
+            int mSeekBarValue;
+    @SuppressWarnings("WeakerAccess")
+        String mUnits;
+    @SuppressWarnings("WeakerAccess")
+            int mMin;
+    private int mMax;
+    private int mSeekBarIncrement;
+    @SuppressWarnings("WeakerAccess")
+            boolean mTrackingTouch;
+    @SuppressWarnings("WeakerAccess")
+            SeslSeekBar mSeekBar;
+    private TextView mSeekBarValueTextView;
+    @SuppressWarnings("WeakerAccess")
+            boolean mAdjustable;
+    private boolean mShowSeekBarValue;
+    @SuppressWarnings("WeakerAccess")
+            boolean mUpdatesContinuously;
+    private SeslSeekBar.OnSeekBarChangeListener mSeekBarChangeListener = new SeslSeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeslSeekBar seekBar, int progress, boolean fromUser) {
+            if (fromUser && (mUpdatesContinuously || !mTrackingTouch)) {
+                syncValueInternal(seekBar);
+            } else {
+                updateLabelValue(progress + mMin);
+            }
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeslSeekBar seekBar) {
+            mTrackingTouch = true;
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeslSeekBar seekBar) {
+            mTrackingTouch = false;
+            if (seekBar.getProgress() + mMin != mSeekBarValue) {
+                syncValueInternal(seekBar);
+            }
+        }
+    };
+    private View.OnKeyListener mSeekBarKeyListener = new View.OnKeyListener() {
+        @Override
+        public boolean onKey(View v, int keyCode, KeyEvent event) {
+            if (event.getAction() != KeyEvent.ACTION_DOWN) {
+                return false;
+            }
+            if (!mAdjustable && (keyCode == KeyEvent.KEYCODE_DPAD_LEFT || keyCode == KeyEvent.KEYCODE_DPAD_RIGHT)) {
+                return false;
+            }
+            if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER || keyCode == KeyEvent.KEYCODE_ENTER) {
+                return false;
+            }
+            if (mSeekBar == null) {
+                LogUtils.e(TAG, "SeekBar view is null and hence cannot be adjusted.");
+                return false;
+            }
+            return mSeekBar.onKeyDown(keyCode, event);
+        }
+    };
+
+
+    public MesaSeekBarPreference(Context context) {
+        this(context, null);
     }
 
-    public MesaSeekBarPreference(Context var1, AttributeSet var2, int var3) {
-        super(var1, var2, var3);
+    public MesaSeekBarPreference(Context context, AttributeSet attrs) {
+        this(context, attrs, R.attr.seekBarPreferenceStyle);
     }
 
-    public MesaSeekBarPreference(Context var1, AttributeSet var2, int var3, int var4) {
-        super(var1, var2, var3, var4);
+    public MesaSeekBarPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public MesaSeekBarPreference(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+
+        setLayoutResource(R.layout.mesa_preference_seekbarpref_layout);
+
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.MesaSeekBarPreference, defStyleAttr, defStyleRes);
+        mMin = a.getInt(R.styleable.MesaSeekBarPreference_min, 0);
+        setMax(a.getInt(R.styleable.MesaSeekBarPreference_max, 100));
+        setSeekBarIncrement(a.getInt(R.styleable.MesaSeekBarPreference_seekBarIncrement, 0));
+        mAdjustable = a.getBoolean(R.styleable.MesaSeekBarPreference_adjustable, true);
+        mShowSeekBarValue = a.getBoolean(R.styleable.MesaSeekBarPreference_showSeekBarValue, false);
+
+        mUnits = a.getString(R.styleable.MesaSeekBarPreference_units);
+        if (mUnits == null)
+            mUnits = "";
+
+        mUpdatesContinuously = a.getBoolean(R.styleable.SeslSeekBarPreference_updatesContinuously, false);
+        a.recycle();
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder view) {
+        super.onBindViewHolder(view);
+        view.itemView.setOnKeyListener(mSeekBarKeyListener);
+        mSeekBar = (SeslSeekBar) view.findViewById(R.id.seekbar);
+        mSeekBarValueTextView = (TextView) view.findViewById(R.id.seekbar_value);
+        if (mShowSeekBarValue) {
+            mSeekBarValueTextView.setVisibility(View.VISIBLE);
+        } else {
+            mSeekBarValueTextView.setVisibility(View.GONE);
+            mSeekBarValueTextView = null;
+        }
+        if (mSeekBar == null) {
+            LogUtils.e(TAG, "SeekBar view is null in onBindViewHolder.");
+            return;
+        }
+
+        try {
+            mSeekBar.setOnSeekBarChangeListener(mSeekBarChangeListener);
+            mSeekBar.setMax(mMax - mMin);
+
+            if (mSeekBarIncrement != 0) {
+                mSeekBar.setKeyProgressIncrement(mSeekBarIncrement);
+            } else {
+                mSeekBarIncrement = mSeekBar.getKeyProgressIncrement();
+            }
+            mSeekBar.setProgress(mSeekBarValue - mMin);
+            updateLabelValue(mSeekBarValue);
+            mSeekBar.setEnabled(isEnabled());
+        } catch (Throwable ignored) { }
+    }
+
+    @Override
+    protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
+        if (defaultValue == null) {
+            defaultValue = 0;
+        }
+        setValue(getPersistedInt((Integer) defaultValue));
+    }
+
+    @Override
+    protected Object onGetDefaultValue(TypedArray a, int index) {
+        return a.getInt(index, 0);
+    }
+
+    public int getMin() {
+        return mMin;
+    }
+
+    public void setMin(int min) {
+        if (min > mMax) {
+            min = mMax;
+        }
+        if (min != mMin) {
+            mMin = min;
+            notifyChanged();
+        }
+    }
+
+    public final int getSeekBarIncrement() {
+        return mSeekBarIncrement;
+    }
+
+    public final void setSeekBarIncrement(int seekBarIncrement) {
+        if (seekBarIncrement != mSeekBarIncrement) {
+            mSeekBarIncrement = Math.min(mMax - mMin, Math.abs(seekBarIncrement));
+            notifyChanged();
+        }
+    }
+
+    public int getMax() {
+        return mMax;
+    }
+
+    public final void setMax(int max) {
+        if (max < mMin) {
+            max = mMin;
+        }
+        if (max != mMax) {
+            mMax = max;
+            notifyChanged();
+        }
+    }
+
+    public boolean isAdjustable() {
+        return mAdjustable;
+    }
+
+    public void setAdjustable(boolean adjustable) {
+        mAdjustable = adjustable;
+    }
+
+    public boolean getUpdatesContinuously() {
+        return mUpdatesContinuously;
+    }
+
+    public void setUpdatesContinuously(boolean updatesContinuously) {
+        mUpdatesContinuously = updatesContinuously;
+    }
+
+    public boolean getShowSeekBarValue() {
+        return mShowSeekBarValue;
+    }
+
+    public void setShowSeekBarValue(boolean showSeekBarValue) {
+        mShowSeekBarValue = showSeekBarValue;
+        notifyChanged();
+    }
+
+    private void setValueInternal(int seekBarValue, boolean notifyChanged) {
+        if (seekBarValue < mMin) {
+            seekBarValue = mMin;
+        }
+        if (seekBarValue > mMax) {
+            seekBarValue = mMax;
+        }
+        if (seekBarValue != mSeekBarValue) {
+            mSeekBarValue = seekBarValue;
+            updateLabelValue(mSeekBarValue);
+            persistInt(seekBarValue);
+            if (notifyChanged) {
+                notifyChanged();
+            }
+        }
+    }
+
+    public int getValue() {
+        return mSeekBarValue;
+    }
+
+    public void setValue(int seekBarValue) {
+        setValueInternal(seekBarValue, true);
+    }
+
+    @SuppressWarnings("WeakerAccess")
+    void syncValueInternal(SeslSeekBar seekBar) {
+        int seekBarValue = mMin + seekBar.getProgress();
+        if (seekBarValue != mSeekBarValue) {
+            if (callChangeListener(seekBarValue)) {
+                setValueInternal(seekBarValue, false);
+            } else {
+                try {
+                    seekBar.setProgress(mSeekBarValue - mMin);
+                    updateLabelValue(mSeekBarValue);
+                } catch (Throwable ignored) { }
+            }
+        }
+    }
+
+    @SuppressWarnings("WeakerAccess")
+    void updateLabelValue(int value) {
+        if (mSeekBarValueTextView != null) {
+            mSeekBarValueTextView.setText(mUnits.isEmpty() ? String.valueOf(value) : value + " " + mUnits);
+        }
+    }
+
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        final Parcelable superState = super.onSaveInstanceState();
+        if (isPersistent()) {
+            return superState;
+        }
+
+        final SavedState myState = new SavedState(superState);
+        myState.mSeekBarValue = mSeekBarValue;
+        myState.mMin = mMin;
+        myState.mMax = mMax;
+        return myState;
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Parcelable state) {
+        if (!state.getClass().equals(SavedState.class)) {
+            super.onRestoreInstanceState(state);
+            return;
+        }
+
+        SavedState myState = (SavedState) state;
+        super.onRestoreInstanceState(myState.getSuperState());
+        mSeekBarValue = myState.mSeekBarValue;
+        mMin = myState.mMin;
+        mMax = myState.mMax;
+        notifyChanged();
+    }
+
+
+    private static class SavedState extends BaseSavedState {
+        public static final Parcelable.Creator<SavedState> CREATOR =
+                new Parcelable.Creator<SavedState>() {
+                    @Override
+                    public SavedState createFromParcel(Parcel in) {
+                        return new SavedState(in);
+                    }
+                    @Override
+                    public SavedState[] newArray(int size) {
+                        return new SavedState[size];
+                    }
+                };
+        int mSeekBarValue;
+        int mMin;
+        int mMax;
+        SavedState(Parcel source) {
+            super(source);
+            mSeekBarValue = source.readInt();
+            mMin = source.readInt();
+            mMax = source.readInt();
+        }
+
+        SavedState(Parcelable superState) {
+            super(superState);
+        }
+
+        @Override
+        public void writeToParcel(Parcel dest, int flags) {
+            super.writeToParcel(dest, flags);
+            dest.writeInt(mSeekBarValue);
+            dest.writeInt(mMin);
+            dest.writeInt(mMax);
+        }
     }
 }
